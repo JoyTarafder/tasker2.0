e2608f3489be5c32935ab1636dca573859b519ab
Joy Tarafder <joytarafder3@gmail.com>
2025-05-12 22:43:17 +0600

    'update'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-      <div>
-        <h1>Tasker</h1>
-        <input type="text" placeholder="Add a task" />
-        <button>Add</button>
-      </div>
+      <Header />
+      <HeroSection />
+      <Footer />
     </>
   );
 }
