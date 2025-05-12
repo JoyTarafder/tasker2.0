cd358498c88ff811db6a21d315a163a93fc681f9
Joy Tarafder <joytarafder3@gmail.com>
2025-05-12 21:56:50 +0600

    'update'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-
-
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <div>
+        <h1>Tasker</h1>
+        <input type="text" placeholder="Add a task" />
+        <button>Add</button>
+      </div>
+    </>
+  );
 }
